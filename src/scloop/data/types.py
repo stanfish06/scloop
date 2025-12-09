4f3c224a7e070b3b5f5fc1cd2787a1aa90726d67
Stan <zyyu@umich.edu>
2025-12-09 14:43:45 -0500

    update: downsample module

--- src/scloop/data/types.py
+++ src/scloop/data/types.py
@@ -1,6 +1,12 @@
 # Copyright 2025 Zhiyuan Yu (Heemskerk's lab, University of Michigan)
-from typing import Literal
+from typing import Literal, Annotated, TypeAlias
+from pydantic import Field
 
 FeatureSelectionMethod = Literal["hvg", "delve", "none"]
 EmbeddingMethod = Literal["pca", "diffmap", "scvi"]
 EmbeddingNeighbors = Literal["pca", "scvi"]
+
+Index_t = Annotated[int, Field(ge=0)]
+# need at least 2 points to compute PH. Maybe also set an upper bound later as it is not feasible to compute PH on a lot of points
+IndexListDownSample: TypeAlias = Annotated[list[Index_t], Field(min_length=2)]
+# TODO: make a type for boundary matrix. Restrict matrix size for efficient computation
--- src/scloop/preprocessing/downsample.py
+++ src/scloop/preprocessing/downsample.py
@@ -1,1 +1,15 @@
-# TODO: use numba
+# Copyright 2025 Zhiyuan Yu (Heemskerk's lab, University of Michigan)
+from numba import jit
+from anndata import AnnData
+from ..data.types import IndexListDownSample
+from pydantic import validate_call
+from typing import TypeAlias
+import numpy as np
+
+@jit
+def _sample_impl(data: np.ndarray, n: int) -> np.ndarray:
+    return np.array([1, 1])
+
+@validate_call(config={"arbitrary_types_allowed": True})
+def sample(adata: AnnData, n: int) -> IndexListDownSample:
+    return _sample_impl(np.array(), n).tolist()
